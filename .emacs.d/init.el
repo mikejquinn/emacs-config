f316f8a28715e1a28cb888bce9ddbf85c5a5b07c
Michael Quinn <mikeq@liftoff.io>
2015-10-29 12:25:18 -0700

    Use ctrl-n / ctrl-p in ido menus

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -573,6 +573,13 @@
      ;; kill the highlighted buffer in the matches list.
      (define-key ido-buffer-completion-map (kbd "M-d") 'ido-kill-buffer-at-head)))
 
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 ;;
 ;; Dired mode - using the Emacs file browser.
 ;;
